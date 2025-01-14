03fe55bc8aa602ed872101692851feebec247ced
Dalianyihao <56790704+zzy0509@users.noreply.github.com>
2025-01-15 01:38:07 +0800

    Delete self-attentive-parser-master directory

--- self-attentive-parser-master/data/spmrl/strip_spmrl_features.py
+++
@@ -1,9 +0,0 @@
-def proc_line(line):
-    line = line.strip()
-    line = "".join(line.split("##")[::2])
-    return line
-
-if __name__ == "__main__":
-    import fileinput
-    for line in fileinput.input():
-        print(proc_line(line))
